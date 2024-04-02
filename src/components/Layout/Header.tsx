123fca6671458b6c09c0f138350b702efee96e74
liemgumball <liem1762001@gmail.com>
2024-04-02 15:38:19 +0700

    feat: auth nav buttons

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,31 @@
+import { Link } from 'react-router-dom'
 import ModeToggle from '../ThemeToggle'
+import { buttonVariants } from '@/components/ui/button'
 
 const Header = () => {
 	return (
-		<header>
+		<header className="flex justify-between px-4">
 			<ModeToggle />
+			<nav className="space-x-4 capitalize">
+				<Link
+					to="/login"
+					className={buttonVariants({
+						variant: 'secondary',
+						size: 'lg',
+					})}
+				>
+					login
+				</Link>
+				<Link
+					to="/signup"
+					className={buttonVariants({
+						variant: 'default',
+						size: 'lg',
+					})}
+				>
+					signup
+				</Link>
+			</nav>
 		</header>
 	)
 }
