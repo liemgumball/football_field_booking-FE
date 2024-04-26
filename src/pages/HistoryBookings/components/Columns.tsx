7d107c15c8781d570c221d3774ba7f0001c5807a
liemgumball <liem1762001@gmail.com>
2024-04-26 11:31:32 +0700

    feat: booking sorted response

--- src/pages/HistoryBookings/components/Columns.tsx
+++ src/pages/HistoryBookings/components/Columns.tsx
@@ -3,6 +3,17 @@ import { ColumnDef } from '@tanstack/react-table'
 import { format } from 'date-fns'
 import { DataTableColumnHeader } from './DataTableColumnHeader'
 import BookingStatus from './BookingStatus'
+import { MoreHorizontal } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Link } from 'react-router-dom'
 
 const Columns: ColumnDef<TBooking, TBooking>[] = [
 	{
@@ -81,6 +92,30 @@ const Columns: ColumnDef<TBooking, TBooking>[] = [
 				{format(cell.getValue() as unknown as string, 'PPP')}
 			</div>
 		),
+	},
+	{
+		id: 'actions',
+		cell: ({ row }) => {
+			const booking = row.original
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem>
+							<Link to={`${booking._id}`}>View booking details</Link>
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>View field details</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
 	},
 ]
 
