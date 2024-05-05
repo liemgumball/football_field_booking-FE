0955e77ac0a9677db1d1a8595621b2156bb5397b
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-06 01:08:53 +0700

    refactor: update responsive for team member section

--- src/pages/AboutUs/TeamMemberSection.tsx
+++ src/pages/AboutUs/TeamMemberSection.tsx
@@ -21,9 +21,9 @@ const Members = [
 const TeamMember = () => {
     return (
         <section className=' space-y-16  container'>
-            <div className='container text-center max-w-[800px]'>
-                <p className={cn(buttonVariants({ size: 'lg' }), 'max-w-max')}> Team Member </p>
-                <h2 className='text-5xl font-bold capitalize tracking-normal mt-5'>we’ve expert team members meet
+            <div className='container text-center max-w-[350px] md:max-w-[600px] lg:max-w-[800px]'>
+                <p className={cn(buttonVariants({ size: 'sm' }), 'max-w-max')}> Team Member </p>
+                <h2 className='text-lg md:text-4xl lg:text-5xl font-bold capitalize tracking-normal mt-5'>we’ve expert team members meet
                     with team</h2>
             </div>
             <ul className='grid grid-cols-1 lg:grid-cols-2 gap-x-4 mt-3 gap-y-5 place-items-center'>
--- src/pages/AboutUs/components/Member.tsx
+++ src/pages/AboutUs/components/Member.tsx
@@ -18,18 +18,18 @@ type MemberProps = {
 const Member = (member: MemberProps) => {
     return (
         <li>
-            <Card className="flex p-8 items-center gap-4">
-                <div>
-                    <img className='rounded-lg' src={member.image} width={90} height={90} alt='avatar member' />
+            <Card className="flex p-4 md:p-8 items-center gap-3 md:gap-5 lg:gap-7">
+                <div className='max-w-[50px] md:max-w-[90px] lg:max-w-[110px]'>
+                    <img className='rounded-lg' src={member.image} alt='avatar member' />
                 </div>
-                <div className='capitalize flex flex-col gap-4 max-w-max '>
-                    <CardHeader className='pb-0'>
-                        <CardTitle className='font-semibold'>{member.name} </CardTitle>
-                        <CardDescription>{member.position}</CardDescription>
+                <div className='capitalize flex flex-col gap-1 max-w-max '>
+                    <CardHeader className='p-1'>
+                        <CardTitle className='font-semibold text-base md:text-lg lg:text-xl'>{member.name} </CardTitle>
+                        <CardDescription className='text-sm md:text-base lg:text-lg'>{member.position}</CardDescription>
                     </CardHeader>
                     <Separator />
 
-                    <CardFooter className='flex space-x-1'>
+                    <CardFooter className='flex p-2 space-x-1'>
                         <li>
                             <SocialContactItem className="fab fa-facebook-f" />
                         </li>
