1374222119f7f4770e40e1655f619da11c08d10b
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-06 01:09:43 +0700

    refactor: update responsive for advantages section

--- src/pages/AboutUs/AdvantagesSection.tsx
+++ src/pages/AboutUs/AdvantagesSection.tsx
@@ -7,25 +7,25 @@ import { cn } from '@/lib/utils'
 
 const Advantages = () => {
     return (
-        <section className="flex flex-col xl:flex-row gap-8 justify-between items-center px-16 xl:px-48">
-            <div className='max-w-[600px] '>
+        <section className="flex flex-col xl:flex-row gap-8 md:gap-12 justify-between items-center px-10 xl:px-48">
+            <div className='max-w-[350px] md:max-w-[650px]'>
                 <div className="space-y-6">
-                    <p className={cn(buttonVariants({ size: 'lg' }), 'flex mx-auto xl:mx-0 max-w-max')}> Who We Are </p>
-                    <h2 className='text-5xl font-bold capitalize text-center xl:text-start'>great opportunity for
+                    <p className={cn(buttonVariants({ size: 'sm' }), 'flex mx-auto xl:mx-0 max-w-max')}> Who We Are </p>
+                    <h2 className='text-xl md:text-4xl lg:text-5xl font-bold capitalize text-center xl:text-start'>great opportunity for
                         adventure & travels</h2>
                 </div>
-                <ul className='space-y-16 mt-16'>
+                <ul className='space-y-8 mt-8'>
                     <FeatureItem />
                     <FeatureItem />
                     <FeatureItem />
                 </ul>
             </div>
             <ul className='grid grid-cols-2 gap-5 max-w-[762px] '>
-                <li className='self-end rounded-xl overflow-hidden'>
-                    <img width={500} src={featureImage2} alt='feature image 2' />
+                <li className='self-end rounded-xl max-w-[100px] md:max-w-[300px] overflow-hidden'>
+                    <img src={featureImage2} alt='feature image 2' />
                 </li>
-                <li >
-                    <img className='rounded-xl' src={featureImage3} alt='feature image 3' />
+                <li className='self-center max-w-[68px] md:max-w-[190px]' >
+                    <img className=' rounded-xl' src={featureImage3} alt='feature image 3' />
                 </li>
                 <li className='col-span-2'>
                     <img className='rounded-xl' src={featureImage1} alt='feature image 1' />
--- src/pages/AboutUs/components/FeatureItem.tsx
+++ src/pages/AboutUs/components/FeatureItem.tsx
@@ -2,13 +2,13 @@ import featureIcon from '/feature-icon.svg'
 
 const FeatureItem = () => {
     return (
-        <li className="flex items-center gap-9">
-            <div className='rounded-full shadow-xl border-solid border-2 p-4'>
-                <img src={featureIcon} alt='feature icon' />
+        <li className="flex items-center gap-3 md:gap-6">
+            <div className='rounded-full shadow-xl border-solid border-2 p-2 '>
+                <img className='max-w-[30px] md:max-w-[80px]' src={featureIcon} alt='feature icon' />
             </div>
             <div>
-                <h3 className="text-3xl font-bold capitalize">safety first always</h3>
-                <p className='text-lg'>Set perspiciatis unde omnis estenatus voluptatem
+                <h3 className="text-base lg:text-xl font-bold capitalize">safety first always</h3>
+                <p className='text-xs md:text-lg'>Set perspiciatis unde omnis estenatus voluptatem
                     accusantium laudantium totarem aperiae</p>
             </div>
         </li>
