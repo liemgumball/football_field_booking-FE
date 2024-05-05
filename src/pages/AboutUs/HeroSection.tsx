f7d8039d548cf3ffeaf4b10ab6bf43243ca651fa
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-06 01:15:09 +0700

    refactor: update responsive for hero section

--- src/pages/AboutUs/HeroSection.tsx
+++ src/pages/AboutUs/HeroSection.tsx
@@ -11,24 +11,26 @@ import {
 const HeroSection = () => {
 
     return (
-        <div className='w-screen h-[500px] bg-hero-about-us bg-cover rounded-lg relative'>
-            <div className='absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed'>
-                <div className="flex flex-col gap-3 h-full items-center justify-center text-white capitalize">
-                    <h1 className='font-bold  text-5xl'>about us</h1>
-                    <Breadcrumb className="rounded-xl bg-background/70 px-8 py-2">
-                        <BreadcrumbList className="text-lg">
-                            <BreadcrumbItem>
-                                <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator />
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>About Us</BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
+        <div className='h-[500px] bg-hero-about-us  bg-cover bg-center relative'>
+            <div className="absolute left-0 top-0 h-full py-56 w-full bg-black bg-opacity-40">
+                <div className='container'>
+                    <div className="flex flex-col gap-3 h-full items-center  text-white capitalize">
+                        <h1 className='font-bold text-xl md:text-3xl lg:text-5xl'>about us</h1>
+                        <Breadcrumb className="rounded-xl bg-background/70 px-8 py-2">
+                            <BreadcrumbList className="text-base md:text-lg">
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>About Us</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+
                 </div>
             </div>
-
         </div>
     )
 
--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -3,7 +3,7 @@ import HeroSection from "./HeroSection"
 import PresentSection from "./PresentSection"
 import TeamMember from "./TeamMemberSection"
 
-const AboutUs = () => <main className="space-y-32 mb-12">
+const AboutUs = () => <main className="space-y-10 md:space-y-14 mb-12">
     <HeroSection />
     <PresentSection />
     <TeamMember />
