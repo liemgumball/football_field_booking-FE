945263fb83215e63395c9f1b742dc0af98a1625a
liemgumball <liem1762001@gmail.com>
2024-05-10 15:31:38 +0700

    fix: init booking search time

--- src/utils/booking.ts
+++ src/utils/booking.ts
@@ -1,7 +1,11 @@
 import { TTurnOfService } from '@/types'
 
 export function getInitialFrom() {
+	// 15 minutes later
 	const now = new Date()
+	now.setTime(now.getTime() + 15 * 60 * 1000)
+
+	// Get next available timestep from 15 minutes later
 	const hours = now.getMinutes() < 30 ? now.getHours() : now.getHours() + 1
 	const minutes = now.getMinutes() < 30 ? '30' : '00'
 	return hours.toString().padStart(2, '0') + ':' + minutes
@@ -9,13 +13,17 @@ export function getInitialFrom() {
 
 export function getInitialTo(from?: string) {
 	if (from) {
+		// return after `from` 1 hour
 		return (
 			(Number(from.slice(0, 2)) + 1).toString().padStart(2, '0') +
 			from.slice(2, 5)
 		)
 	}
-
+	// 15 minutes later
 	const now = new Date()
+	now.setTime(now.getTime() + 15 * 60 * 1000)
+
+	// Get next available timestep from 15 minutes later
 	const minutes = now.getMinutes() < 30 ? '30' : '00'
 	const hours = now.getMinutes() < 30 ? now.getHours() : now.getHours() + 1
 	return (hours + 1).toString().padStart(2, '0') + ':' + minutes
