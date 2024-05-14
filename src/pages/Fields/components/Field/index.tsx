393a63f51e9cf407aad83ae38a2401ed5999fcf5
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-14 11:24:16 +0700

    refactor(list field): update responsive for field card

--- src/pages/Fields/components/Field/index.tsx
+++ src/pages/Fields/components/Field/index.tsx
@@ -9,7 +9,7 @@ const FieldCard = ({
 }: Pick<TFootballField, 'images' | 'name' | 'rating'>) => {
 	return (
 		<div className="relative max-h-[227px] overflow-hidden">
-			<div className="max-w-max opacity-55">
+			<div className="max-w-max opacity-80">
 				<img
 					width={300}
 					className="rounded"
@@ -17,12 +17,12 @@ const FieldCard = ({
 					alt="image field"
 				/>
 			</div>
-			<div className="absolute right-5 top-5 flex gap-2 rounded bg-primary p-2">
+			<div className="absolute right-5 top-5 flex gap-2 rounded bg-primary p-2 md:right-4 lg:right-5 lg:top-5">
 				<p> {rating || 'No'} </p>
 				<StarIcon />
 			</div>
 			<div className="absolute left-0 top-28 flex max-h-80 w-full items-end px-5 pb-5 pt-4">
-				<h3 className="text-xl font-bold">{name}</h3>
+				<h3 className="text-base font-bold md:text-lg lg:text-xl">{name}</h3>
 			</div>
 		</div>
 	)
