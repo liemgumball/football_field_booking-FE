50292c4df8e44d87913d14fffa0714dd8540488d
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-14 11:19:46 +0700

    refactor(list field): update responsive for search form

--- src/pages/Fields/components/Form/SearchByNameForm.tsx
+++ src/pages/Fields/components/Form/SearchByNameForm.tsx
@@ -6,9 +6,9 @@ const SearchByNameForm = ({
 	setName: React.Dispatch<React.SetStateAction<string>>
 }) => {
 	return (
-		<form className="relative">
+		<form className="relative text-center lg:text-start">
 			<input
-				className="appearance-none border-2 border-solid border-green-600 px-6 py-4 text-lg text-black outline-none"
+				className="appearance-none border-2 border-solid border-green-600 px-2 py-1 text-base text-black outline-none md:px-4 md:py-2 lg:px-6 lg:py-4 lg:text-lg"
 				type="text"
 				placeholder="Search Here"
 				maxLength={20}
@@ -16,7 +16,7 @@ const SearchByNameForm = ({
 					setName(e.target.value)
 				}}
 			/>
-			<button className="absolute right-2 top-5 text-black">
+			<button className="absolute left-52 top-1.5 text-black md:left-[430px] md:top-2.5 lg:right-2 lg:top-5">
 				<SearchIcon />
 			</button>
 		</form>
