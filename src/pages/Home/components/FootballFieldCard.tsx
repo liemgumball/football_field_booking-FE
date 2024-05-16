600a650e16503d1cfaafb4798ad05192d85c4234
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-16 23:54:22 +0700

    fix(final project): tag link wrap field card

--- src/pages/Home/components/FootballFieldCard.tsx
+++ src/pages/Home/components/FootballFieldCard.tsx
@@ -20,40 +20,37 @@ const FootballFieldCard = ({
 		: 'https://demo.webtend.net/html/gowilds/assets/images/features/feat-2.jpg' // just fallback cause the api already get fields with images
 
 	return (
-		<figure className="group relative h-[480px] w-full overflow-hidden rounded-lg">
-			<img
-				src={imgSrc}
-				alt="field image"
-				className="h-full w-full object-cover"
-			/>
+		<Link to={`/fields/${_id}`}>
+			<figure className="group relative h-[480px] w-full overflow-hidden rounded-lg">
+				<img
+					src={imgSrc}
+					alt="field image"
+					className="h-full w-full object-cover"
+				/>
 
-			<figcaption className="absolute bottom-4 left-8 right-8">
-				<Card className="p-2">
-					<CardHeader className="flex justify-between gap-2">
-						<CardTitle className="truncate text-base">
-							<Link
-								to={`/fields/${_id}`}
-								className="transition hover:text-primary"
-							>
-								{name}
-							</Link>
-						</CardTitle>
-						<CardDescription className="flex space-x-1">
-							{rating
-								? Array.from({ length: rating }, (_, i) => i).map((item) => (
-										<Star key={item} size={15} color="orange" />
-									))
-								: 'No rating'}
-						</CardDescription>
-					</CardHeader>
-					<CardContent className="h-auto max-h-0 overflow-hidden py-0 opacity-0 transition-all delay-100 duration-700 group-hover:mb-4 group-hover:max-h-[90px] group-hover:opacity-100">
-						<p className="text-xs text-muted-foreground">
-							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-						</p>
-					</CardContent>
-				</Card>
-			</figcaption>
-		</figure>
+				<figcaption className="absolute bottom-4 left-8 right-8">
+					<Card className="p-2">
+						<CardHeader className="flex justify-between gap-2">
+							<CardTitle className="truncate text-base">
+								<p className="transition hover:text-primary">{name}</p>
+							</CardTitle>
+							<CardDescription className="flex space-x-1">
+								{rating
+									? Array.from({ length: rating }, (_, i) => i).map((item) => (
+											<Star key={item} size={15} color="orange" />
+										))
+									: 'No rating'}
+							</CardDescription>
+						</CardHeader>
+						<CardContent className="h-auto max-h-0 overflow-hidden py-0 opacity-0 transition-all delay-100 duration-700 group-hover:mb-4 group-hover:max-h-[90px] group-hover:opacity-100">
+							<p className="text-xs text-muted-foreground">
+								Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							</p>
+						</CardContent>
+					</Card>
+				</figcaption>
+			</figure>
+		</Link>
 	)
 }
 
