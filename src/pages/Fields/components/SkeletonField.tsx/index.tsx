6bb0c3cdcc6d04ef0957abe10b4d8546b2cb958d
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-20 17:11:55 +0700

    refactor(skeleton): update size for skeleton

--- src/pages/Fields/components/SkeletonField.tsx/index.tsx
+++ src/pages/Fields/components/SkeletonField.tsx/index.tsx
@@ -4,8 +4,8 @@ const SkeletonField = () => {
 	return (
 		<div className="relative h-[300px] w-[300px]">
 			<Skeleton className="relative h-[300px] w-[300px] rounded-xl" />
-			<div className="absolute bottom-8 left-2">
-				<Skeleton className="h-24 w-[280px]" />
+			<div className="absolute bottom-5 left-7 ">
+				<Skeleton className="h-[124px] w-[236px]" />
 			</div>
 		</div>
 	)
