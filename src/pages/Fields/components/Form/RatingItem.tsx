bcb5e41a5bb6cbec2cdd7cb8948bde5889d67582
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-20 14:52:43 +0700

    refactor(filter rating): update star icon for filter rating

--- src/pages/Fields/components/Form/RatingItem.tsx
+++ src/pages/Fields/components/Form/RatingItem.tsx
@@ -5,21 +5,29 @@ const RatingItem = ({
 	isChecked,
 	onClick,
 }: {
-	variant: number
+	variant: string
 	isChecked: boolean
 	onClick: () => void
 }) => {
 	return (
 		<div className="flex  flex-row items-center gap-2 lg:gap-4">
-			<input type="radio" checked={isChecked} onClick={onClick} />
-			<label className="flex gap-1 text-base uppercase lg:gap-2 lg:text-lg">
-				{variant}
+			<input type="radio" id={variant} checked={isChecked} onClick={onClick} />
+			<label
+				htmlFor={variant}
+				className="flex gap-1 text-base uppercase lg:gap-2 lg:text-lg"
+			>
 				<span className="flex">
-					{Array(Math.floor(variant))
+					{Array(Math.floor(5))
 						.fill(null)
-						.map((_, index) => (
-							<StarIcon key={index} color="orange" />
-						))}
+						.map((_, index) =>
+							index + 1 <= parseInt(variant) ? (
+								<StarIcon color="orange" key={index} />
+							) : (
+								<div className="text-muted-foreground">
+									<StarIcon key={index} />
+								</div>
+							),
+						)}
 				</span>
 			</label>
 		</div>
--- src/pages/Fields/components/Form/RatingOption.tsx
+++ src/pages/Fields/components/Form/RatingOption.tsx
@@ -12,14 +12,22 @@ const RatingOption = ({
 	return (
 		<div className="mt-1 border-solid border-green-600 pb-5 pt-2 lg:mt-3 lg:border-b-2">
 			<p className="mb-2 text-lg capitalize lg:text-xl">rating</p>
-			<div className="flex flex-col gap-4 pl-4 md:flex-row lg:flex-col">
+			<div className="flex flex-col gap-4 pl-4">
+				<RatingItem
+					isChecked={rating === 5}
+					onClick={() => {
+						if (rating === 5) setRating(undefined)
+						else setRating(5)
+					}}
+					variant="5"
+				/>
 				<RatingItem
 					isChecked={rating === 4}
 					onClick={() => {
 						if (rating === 4) setRating(undefined)
 						else setRating(4)
 					}}
-					variant={4}
+					variant="4"
 				/>
 				<RatingItem
 					isChecked={rating === 3}
@@ -27,7 +35,7 @@ const RatingOption = ({
 						if (rating === 3) setRating(undefined)
 						else setRating(3)
 					}}
-					variant={3}
+					variant="3"
 				/>
 				<RatingItem
 					isChecked={rating === 2}
@@ -35,7 +43,7 @@ const RatingOption = ({
 						if (rating === 2) setRating(undefined)
 						else setRating(2)
 					}}
-					variant={2}
+					variant="2"
 				/>
 				<RatingItem
 					isChecked={rating === 1}
@@ -43,7 +51,7 @@ const RatingOption = ({
 						if (rating === 1) setRating(undefined)
 						else setRating(1)
 					}}
-					variant={1}
+					variant="1"
 				/>
 			</div>
 		</div>
