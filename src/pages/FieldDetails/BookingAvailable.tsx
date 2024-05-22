c7a236439a4b43ed4b74909d9fcfb211ab3eef43
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-22 17:39:08 +0700

    feat(booking subfield): init booking subfields

---
+++ src/pages/FieldDetails/BookingAvailable.tsx
@@ -0,0 +1,11 @@
+import AvailabilityForm from '@/components/AvailabilityForm'
+
+const BookingAvailable = () => {
+	return (
+		<div>
+			<AvailabilityForm />
+		</div>
+	)
+}
+
+export default BookingAvailable
