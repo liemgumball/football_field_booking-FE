263f5fcfe6b0c21b2dc079c7a02eb920a805e217
liemgumball <liem1762001@gmail.com>
2024-05-24 15:26:56 +0700

    refactor: google login button & loader

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,70 +1,29 @@
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import useDocumentTitle from '@/hooks/useDocumentTitle'
 
 import LoginForm from './components/LoginForm'
-import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/button'
 import { PATHS } from '@/constants/navigation'
-import { Separator } from '@/components/ui/separator'
-import useDocumentTitle from '@/hooks/useDocumentTitle'
-import { googleLogin } from '@/services/user'
-import { toast } from '@/components/ui/use-toast'
-import useAuthStore from '@/stores/auth'
+import GoogleLoginButton from '@/components/GoogleLoginButton'
 
 const Login = () => {
 	useDocumentTitle('Login')
-	const setAuth = useAuthStore((state) => state.set)
-	const navigate = useNavigate()
-
-	const onSuccess = async (res: CredentialResponse) => {
-		try {
-			if (!res.credential) throw new Error('Fail to login with google account.')
-
-			const response = await googleLogin({
-				credential: res.credential,
-			})
-			if (response) {
-				setAuth(response)
-				navigate('/')
-			}
-		} catch (error) {
-			if (error instanceof Error)
-				toast({ title: error.message, variant: 'destructive' })
-
-			if (error instanceof Response) {
-				toast({ title: await error.text() })
-			}
-		}
-	}
 
 	return (
-		<main className="container my-8 flex items-center lg:flex-row">
-			{/* <div className="hidden w-full min-w-[500px] xl:block">
-				<img src={loginImage} alt="login image" />
-			</div> */}
-			<div className="flex w-full flex-col items-center space-y-7 px-0 py-8 lg:px-14">
+		<main className="container my-8 text-center">
+			<div className="container max-w-max space-y-7 border-b-2 pb-4 text-center">
 				<h2 className="text-3xl font-bold capitalize">login</h2>
 				<LoginForm />
 				<p className="text-center font-medium">
 					Not a member?{' '}
 					<Link
 						to={PATHS.SIGNUP}
-						className={cn(
-							buttonVariants({ variant: 'link' }),
-							'mr-1 px-0 text-base font-medium capitalize text-primary',
-						)}
+						className="mr-2 text-primary underline-offset-2 hover:underline"
 					>
-						sign up
-					</Link>{' '}
+						Sign up
+					</Link>
 				</p>
-				<Separator />
-				<GoogleLogin
-					size="large"
-					width={780}
-					onSuccess={onSuccess}
-					text="signin_with"
-				/>
 			</div>
+			<GoogleLoginButton />
 		</main>
 	)
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,53 +1,19 @@
-import { Separator } from '@/components/ui/separator'
+import GoogleLoginButton from '@/components/GoogleLoginButton'
 import SignUpForm from './components/SignUpForm'
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
 import useDocumentTitle from '@/hooks/useDocumentTitle'
-import useAuthStore from '@/stores/auth'
-import { useNavigate } from 'react-router-dom'
-import { googleLogin } from '@/services/user'
-import { toast } from '@/components/ui/use-toast'
 
 const SignUp = () => {
 	useDocumentTitle('Sign Up')
-	const setAuth = useAuthStore((state) => state.set)
-	const navigate = useNavigate()
-
-	const onSuccess = async (res: CredentialResponse) => {
-		try {
-			if (!res.credential) throw new Error('Fail to login with google account.')
-
-			const response = await googleLogin({
-				credential: res.credential,
-			})
-			if (response) {
-				setAuth(response)
-				navigate('/')
-			}
-		} catch (error) {
-			if (error instanceof Error)
-				toast({ title: error.message, variant: 'destructive' })
-
-			if (error instanceof Response) {
-				toast({ title: await error.text() })
-			}
-		}
-	}
 
 	return (
-		<main className="container my-8 flex items-center lg:flex-row">
-			<div className="flex w-full flex-col items-center space-y-7 px-0 py-8 lg:px-14">
-				<h2 className="text-center text-3xl font-bold md:text-4xl">
+		<main className="container mt-8">
+			<div className="container max-w-max space-y-7 border-b-2 pb-3 text-center">
+				<h2 className="text-3xl font-bold capitalize">
 					Create your new account
 				</h2>
 				<SignUpForm />
-				<Separator />
-				<GoogleLogin
-					size="large"
-					width={780}
-					onSuccess={onSuccess}
-					text="signup_with"
-				/>
 			</div>
+			<GoogleLoginButton />
 		</main>
 	)
 }
