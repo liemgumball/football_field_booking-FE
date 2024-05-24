b18ff0618a2961e0b9c6d8a7965207a0d47e0cf9
liemgumball <liem1762001@gmail.com>
2024-05-24 11:51:23 +0700

    fix: login & signup pages

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,6 @@
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
 import { Link } from 'react-router-dom'
 
-import loginImage from '/imageLogin.png'
 import LoginForm from './components/LoginForm'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
@@ -19,9 +18,9 @@ const Login = () => {
 
 	return (
 		<main className="container my-8 flex items-center lg:flex-row">
-			<div className="hidden w-full min-w-[500px] xl:block">
+			{/* <div className="hidden w-full min-w-[500px] xl:block">
 				<img src={loginImage} alt="login image" />
-			</div>
+			</div> */}
 			<div className="flex w-full flex-col items-center space-y-7 px-0 py-8 lg:px-14">
 				<h2 className="text-3xl font-bold capitalize">login</h2>
 				<LoginForm />
@@ -42,7 +41,7 @@ const Login = () => {
 					size="large"
 					width={780}
 					onSuccess={onSuccess}
-					text="signin"
+					text="signin_with"
 				/>
 			</div>
 		</main>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,5 @@
 import { Separator } from '@/components/ui/separator'
 import SignUpForm from './components/SignUpForm'
-import signupImg from '/signupImg.png'
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
 import useDocumentTitle from '@/hooks/useDocumentTitle'
 
@@ -14,9 +13,6 @@ const SignUp = () => {
 
 	return (
 		<main className="container my-8 flex items-center lg:flex-row">
-			<div className="hidden h-auto max-w-[780px] xl:block">
-				<img src={signupImg} alt="signup image" />
-			</div>
 			<div className="flex w-full flex-col items-center space-y-7 px-0 py-8 lg:px-14">
 				<h2 className="text-center text-3xl font-bold md:text-4xl">
 					Create your new account
