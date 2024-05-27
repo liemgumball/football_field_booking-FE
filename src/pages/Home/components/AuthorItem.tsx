e84dc1b5f3f601cfb2b486e2dabb7335adc21566
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-05-27 17:33:50 +0700

    fix(review section): update user avatar

--- src/pages/Home/components/AuthorItem.tsx
+++ src/pages/Home/components/AuthorItem.tsx
@@ -1,14 +1,18 @@
-import authImg from '/author-1.jpg'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import useAuthStore from '@/stores/auth'
 
-const AuthorItem = () => {
+const AuthorItem = ({ name, email }: { name: string; email: string }) => {
+	const user = useAuthStore((set) => set.user)
+
 	return (
 		<div className="flex gap-6">
+			<Avatar className="min-h-[60px] min-w-[60px]">
+				<AvatarImage src={user?.avatar} alt="author" />
+				<AvatarFallback>HP</AvatarFallback>
+			</Avatar>
 			<div>
-				<img src={authImg} className="rounded-full" alt="author" />
-			</div>
-			<div>
-				<h3 className="mb-3 text-2xl font-semibold"> Douglas D. Hall</h3>
-				<p className="text-xl"> CEO & Founder</p>
+				<h3 className="mb-1 text-2xl font-semibold">{name} </h3>
+				<p className="text-sm"> {email}</p>
 			</div>
 		</div>
 	)
