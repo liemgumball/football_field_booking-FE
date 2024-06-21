3ba9807464d7d7b4050a0a568c4c3a9c1e7d88ba
Hoang Phuong Le <lephuonghoang198@gmail.com>
2024-06-22 01:11:58 +0700

    refactor(navbar): add tag link for introduction

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -38,7 +38,7 @@ const NavBar = () => {
 			<NavigationMenuList>
 				<NavigationMenuItem>
 					<NavigationMenuTrigger className="capitalize">
-						Introduction
+						<Link to={PATHS.HOME}>Introduction</Link>
 					</NavigationMenuTrigger>
 					<NavigationMenuContent>
 						<ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
